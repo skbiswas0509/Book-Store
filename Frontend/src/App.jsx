59475fda04789973249aac65572d1be97e754e06
skbiswas0509 <sukumarbsws98@gmail.com>
2025-01-30 09:54:44 -0600

    1.00

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import FreeBooks from './components/FreeBooks'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+      <FreeBooks/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/FreeBooks.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import list from "../../public/list.json"
+
+const FreeBooks = () => {
+  return (
+    <div>FreeBooks</div>
+  )
+}
+
+export default FreeBooks
